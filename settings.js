647e27c181129f4c8673d49fd6236f05b999a755
Sanchitha0423bot <ffirejiro@gmail.com>
2024-08-03 18:18:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,7 +32,7 @@ global.language = english //change indonesia to english if you don't understand 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.ownername = "SACHIN-MD" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
@@ -40,8 +40,8 @@ global.email = "mdsandaru2@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
 global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
+global.owner = ["702722766", "94764246070"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "702722766" // nomor wa kalian
 global.ownernomerr = "+94764497078" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
